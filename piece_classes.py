b103794b6f91044576dacc260666a11f718c4a53
moolordking <36748051+moolordking@users.noreply.github.com>
2022-05-11 19:40:28 +0100

    Update piece_classes.py

--- piece_classes.py
+++ piece_classes.py
@@ -1,8 +1,14 @@
 
-class piece(object):
-    def __init__(self, type, col):
-        self.type = type
+class Piece(object):
+    def __init__(self, piece_type, col):
+        self.piece_type = piece_type
         self.col = col
         self.position = [0,0]
-    def set_position(pos):
+    def set_position(self,pos):
         self.position = [pos[0], pos[1]]
+    def show_moves(self):
+        return None
+
+if __name__ == "__main__":
+    my_piece = Piece("king", 0)
+    print((my_piece.col*"black "or"white ") + my_piece.piece_type)
