e2dc05c3cf069595db0a3a80c670d9520de0641a
moolordking <36748051+moolordking@users.noreply.github.com>
2022-05-11 19:34:39 +0100

    Update piece_classes.py

---
+++ piece_classes.py
@@ -0,0 +1,8 @@
+
+class piece(object):
+    def __init__(self, type, col):
+        self.type = type
+        self.col = col
+        self.position = [0,0]
+    def set_position(pos):
+        self.position = [pos[0], pos[1]]
